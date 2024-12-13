fe6b4e9f15488ccfc66b66256e3344852357e3f4
pvn-a <pavan.leo36dna@gmail.com>
2024-12-13 02:43:01 -0700

    add deployment scripts, backend and kafka are not working

--- frontend/src/components/config.js
+++ frontend/src/components/config.js
@@ -1,5 +1,6 @@
 const config = {
     apiUrl: process.env.BACKEND_URL || "http://localhost:6767",
+    socketUrl: process.env.BACKEND_URL_WS || "ws://localhost:6767"
   };
   
   export default config;
