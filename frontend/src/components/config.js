f34ee5a800c3688c239503aa27221613541aa3dd
pvn-a <pavan.leo36dna@gmail.com>
2024-12-13 03:46:51 -0700

    make some minor changes

--- frontend/src/components/config.js
+++ frontend/src/components/config.js
@@ -1,6 +1,6 @@
 const config = {
-    apiUrl: process.env.BACKEND_URL || "http://localhost:6767",
-    socketUrl: process.env.BACKEND_URL_WS || "ws://localhost:6767"
+    apiUrl: process.env.REACT_APP_BACKEND_URL,
+    socketUrl: process.env.REACT_APP_BACKEND_URL_WS
   };
   
   export default config;
