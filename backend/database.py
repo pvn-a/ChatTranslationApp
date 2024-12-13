815f63a0744cda4fa21970b4a75a1f2a19e00609
pvn-a <pavan.leo36dna@gmail.com>
2024-12-13 01:32:12 -0700

    removed hardcoding of localhost

--- backend/database.py
+++ backend/database.py
@@ -29,5 +29,5 @@ REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
 redis_client = redis.StrictRedis.from_url(REDIS_URL, decode_responses=True)
 
 # Kafka Configuration
-KAFKA_BROKER = "kafka:9092"  # Use Kafka service name in docker-compose
+KAFKA_BROKER = os.getenv("KAFKA_BROKER", "redis://localhost:6379/0")  # Use Kafka service name in docker-compose
 producer = Producer({"bootstrap.servers": KAFKA_BROKER})
